74d867bd55d335d2e856ee8014189044713f9bad
Balthazar7z <635895003@qq.com>
2025-11-08 23:19:32 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -51,4 +51,4 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 # 统一文件共享
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES webdav2 luci-app-unishare"
 # 删除build24.sh构建预装插件
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-passwall-zh-cn -luci-i18n-homeproxy-zh-cn -luci-i18n-dufs-zh-cn -luci-app-openclash"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-passwall-zh-cn -luci-i18n-homeproxy-zh-cn -luci-i18n-dufs-zh-cn"
